e2f3782b6e0dc5f68e20c059e1274cea0a0c3a95
Usman <usman26282@gmail.com>
2023-02-21 09:11:12 -0800

    TAsk0-8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	write(2, "and that place of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	return (1);
+}
